5edc257e049c1a8d51ba514f4d4d08c280033467
Andres Dominguez <adominguezl800@gmail.com>
2022-12-04 20:03:39 -0800

    git commit -m "Added Account class and modified Customer class"

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -21,33 +21,40 @@ public class Customer {
   public int getId() {
     return id;
   }
+
   public void setId(int id) {
     this.id = id;
   }
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public String getUsername() {
     return username;
   }
+
   public void setUsername(String username) {
     this.username = username;
   }
+
   public String getPassword() {
     return password;
   }
+
   public void setPassword(String password) {
     this.password = password;
   }
+
   public int getAccountId() {
     return accountId;
   }
+
   public void setAccountId(int accountId) {
     this.accountId = accountId;
   }
-
   
 }
